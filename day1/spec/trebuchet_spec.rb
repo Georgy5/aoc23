ed068baa1dfef13376eb8c27a45e9ffa8f442045
Karl Keller <kgk86@hotmail.com>
2023-12-01 23:49:07 +0100

    Update trebuchet_spec - remove subject

--- day1/spec/trebuchet_spec.rb
+++ day1/spec/trebuchet_spec.rb
@@ -4,19 +4,19 @@ require_relative "../trebuchet.rb"
 describe "#trebuchet" do
   describe "Find calibration value" do
     it "return a two-digit number" do
-      expect(subject.trebuchet("1abc2")).to eq(12)
+      expect(trebuchet("1abc2")).to eq(12)
     end
 
     it "return a two-digit number" do
-      expect(subject.trebuchet("pqr3stu8vwx")).to eq(38)
+      expect(trebuchet("pqr3stu8vwx")).to eq(38)
     end
 
     it "return a two-digit number" do
-      expect(subject.trebuchet("a1b2c3d4e5f")).to eq(15)
+      expect(trebuchet("a1b2c3d4e5f")).to eq(15)
     end
 
     it "return a two-digit number" do
-      expect(subject.trebuchet("treb7uchet")).to eq(77)
+      expect(trebuchet("treb7uchet")).to eq(77)
     end
   end
 end
