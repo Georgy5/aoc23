f5fa42d610257c2e2e431789143e2f4666e397fa
Karl Keller <kgk86@hotmail.com>
2023-12-02 00:59:06 +0100

    Create dictionary of numbers 1-9 as written digits

--- day1/trebuchet.rb
+++ day1/trebuchet.rb
@@ -1,5 +1,17 @@
 # frozen_string_literal: true
 
+NUMBERS = {
+  one: 1,
+  two: 2,
+  three: 3,
+  four: 4,
+  five: 5,
+  six: 6,
+  seven: 7,
+  eight: 8,
+  nine: 9
+}.freeze
+
 def trebuchet(string)
   "#{string[/\d/]}#{string.reverse[/\d/]}".to_i
 end
