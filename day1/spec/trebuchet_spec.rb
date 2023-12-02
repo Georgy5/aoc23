22232586cee49bf17996bf8580f38eb3cf24108a
Karl Keller <kgk86@hotmail.com>
2023-12-02 02:38:46 +0100

    Combine Specs - only test #trebuchet

--- day1/spec/trebuchet_spec.rb
+++ day1/spec/trebuchet_spec.rb
@@ -18,9 +18,35 @@ describe "#trebuchet" do
     it "return a two-digit number" do
       expect(trebuchet("treb7uchet")).to eq(77)
     end
+  end
 
+  describe "Find written calibration value" do
     it "return a two-digit number" do
       expect(trebuchet("two1nine")).to eq(29)
+    end
+
+    it "return a two-digit number" do
+      expect(trebuchet("eightwothree")).to eq(83)
+    end
+
+    it "return a two-digit number" do
+      expect(trebuchet("abcone2threexyz")).to eq(13)
+    end
+
+    it "return a two-digit number" do
+      expect(trebuchet("xtwone3four")).to eq(24)
+    end
+
+    it "return a two-digit number" do
+      expect(trebuchet("4nineeightseven2")).to eq(42)
+    end
+
+    it "return a two-digit number" do
+      expect(trebuchet("zoneight234")).to eq(14)
+    end
+
+    it "return a two-digit number" do
+      expect(trebuchet("7pqrstsixteen")).to eq(76)
     end
   end
 end
